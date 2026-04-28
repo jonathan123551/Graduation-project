220a63dba37c701b6e7e3e1af2ea49df2bc9851c
Jonathan <jonathanmaged80@gmail.com>
2026-04-28 21:12:24 +0300

    remove env from production

--- idevest-backend/routes/web.php
+++ idevest-backend/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function () {
-    return file_get_contents(public_path('index.html'));
-})->where('any', '.*');
+Route::get('/', function () {
+    return 'IDEVEST WORKING';
+});
